8f2f539a098ca9b091951e77eee7e8200815f8ea
alexanderushe <alex.f.ushe@gmail.com>
2023-09-07 00:15:31 +0200

    updated login required for flask, current_user

--- blueprints/nurse/main.py
+++ blueprints/nurse/main.py
@@ -1,10 +1,17 @@
-from Blueprint.nurse import nurse
+from  blueprints.nurse import nurse
 from flask import render_template, redirect, url_for, flash
+from __main__ import control
+from flask_login import current_user, login_requied
+
+from mysql_engine.appointment import Appointment
+from mysql_engine.patient import Patient
 
 @nurse.route('/dashboard')
+@login_required
 def dashboard():
-    assigned_patients = Patient.query.filter_by(nurse_id= current_user.id).all()
-    upcoming_appointments = Appointment.query.filter_by(nurse_id= current_user.id).all()
+    current_user_id = current_user.id #this will be based on the current user logged in
+    assigned_patients = Patient.query.filter_by(nurse_id= current_user_id).all()
+    upcoming_appointments = Appointment.query.filter_by(nurse_id= current_user_id).all()
 
     return render_template('nurse/dashboard.html', patients=assigned_patients, appointments=upcoming_appointments)
 
@@ -32,8 +39,8 @@ def book():
             mark_doctor_unavailable(doctor, appointment_time)
 
             #save the appointment and update the doctor's availability
-            db.session.add(appointment)
-            db.session.commit()
+            control.add_item(appointment)
+            control.commit_session()
             flash('Appointment scheduled successfully', 'success')
             return redirect(url_for('nurse.dashboard'))
         else:
@@ -41,8 +48,8 @@ def book():
     render_template('nurse/book.html', form=form)
 
     def is_doctor_available(doctor, appointment_time):
-        exisitng_appointment = Appointment.query.filter_by(doctor_id=doctor.id, appointment_time=appointment_time).first()
-        return exisitng_appointment is None
-    def mark_doctor_available(doctor, appointment_time):
+        existing_appointment = Appointment.query.filter_by(doctor_id=doctor.id, appointment_time=appointment_time).first()
+        return existing_appointment is None
+    def mark_doctor_unavailable(doctor, appointment_time):
         doctor.availability.append(appointment_time)
-        db.session.commit()
+        control.commit_session()
